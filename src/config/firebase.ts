743af5e980973220ad87dc17fd9a79c632771e8a
Jeiman Cabarcas <jeimancabarcas@gmail.com>
2025-02-14 11:29:59 -0500

    Removing logs

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,13 +2,11 @@ import * as admin from "firebase-admin";
 import { getFirestore } from "firebase-admin/firestore";
 import "dotenv/config";
 
-console.log(process.env.FIREBASE_PRIVATE_KEY)
 const serviceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
 };
-console.log(serviceAccount.privateKey)
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
