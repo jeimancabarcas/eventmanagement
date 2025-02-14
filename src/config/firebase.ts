25771fa474a8b3c10b3fdaf291dd32e3133fb34b
Jeiman Cabarcas <jeimancabarcas@gmail.com>
2025-02-14 11:19:47 -0500

    Format private key

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -10,7 +10,7 @@ console.log(process.env.FIREBASE_PRIVATE_KEY)
 const serviceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/^"|"$/g, ''),
 };
 console.log(serviceAccount.privateKey)
 admin.initializeApp({
