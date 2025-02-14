ed93d65ae2cd69b22c24b8251ff296678778291a
Jeiman Cabarcas <jeimancabarcas@gmail.com>
2025-02-14 10:46:33 -0500

    Private key format removed

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -9,7 +9,7 @@ if (!process.env.FIREBASE_PRIVATE_KEY) {
 const serviceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  privateKey: process.env.FIREBASE_PRIVATE_KEY,
 };
 
 admin.initializeApp({
