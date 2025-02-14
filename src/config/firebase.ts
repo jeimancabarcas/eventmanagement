b157aadc6ae65cbd1f4a284c06f60a75848beb2e
Jeiman Cabarcas <jeimancabarcas@gmail.com>
2025-02-14 11:00:53 -0500

    log added

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -11,7 +11,7 @@ const serviceAccount = {
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
 };
-
+console.log(serviceAccount)
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
